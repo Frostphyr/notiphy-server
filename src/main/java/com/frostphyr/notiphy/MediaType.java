d6a8e01324b3bc89dbb2096392e228e938f5ae1f
Omoshu <omoshulol@gmail.com>
2018-10-28 18:20:26 -0400

    Added relay for Twitter

--- src/main/java/com/frostphyr/notiphy/MediaType.java
+++ src/main/java/com/frostphyr/notiphy/MediaType.java
@@ -2,10 +2,47 @@ package com.frostphyr.notiphy;
 
 public enum MediaType {
 	
-	OPTIONAL,
-	ANY,
-	NONE,
-	PICTURE,
-	VIDEO
+	NONE(2),
+	IMAGE(3),
+	VIDEO(4),
+	ANY(1, IMAGE, VIDEO),
+	OPTIONAL(0, NONE, IMAGE, VIDEO, ANY);
+	
+	private static final MediaType[] types = new MediaType[values().length];
+	
+	private final int id;
+	private final MediaType[] validTypes;
+	
+	static {
+		for (MediaType t : values()) {
+			types[t.id] = t;
+		}
+	}
+	
+	private MediaType(int id, MediaType... validTypes) {
+		this.id = id;
+		this.validTypes = validTypes;
+	}
+	
+	public static MediaType forId(int id) {
+		return id < 0 || id >= types.length ? null : types[id];
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public boolean isValidFor(MediaType type) {
+		if (type == this) {
+			return true;
+		}
+		
+		for (MediaType t : validTypes) {
+			if (t == type) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
